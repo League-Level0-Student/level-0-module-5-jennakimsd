8f89252eb39db887accd385b6f5113d9cfc0bb73
SunStar56 <league@WTS22.attlocal.net>
2018-10-15 16:56:09 -0700

    I did Fibonacci, Guessing Pi, and Circle Ring

--- src/nested_loops/CircleRing.java
+++ src/nested_loops/CircleRing.java
@@ -7,29 +7,37 @@ package nested_loops;
 import org.jointheleague.graphical.robot.Robot;
 
 public class CircleRing {
-    public static void main(String[] args) throws Exception {
+	public static void main(String[] args) throws Exception {
 
-        // 1. Make a new Robot
+		// 1. Make a new Robot
+		Robot sami = new Robot();
 
+		// 2. Set your robot’s position to x=150 and y=200
+		sami.setX(150);
+		sami.setY(200);
+		// 3. Put the robot's pen down
+		sami.penDown();
+		// 4. Set the robot’s speed to 10
+		sami.setSpeed(100);
+		// 5. Do everything below here 360 times (use i as the counter)
+		for (int i = 0; i < 360; i++) {
 
-       //  2. Set your robot’s position to x=150 and y=200
+			// 6. Move the robot 3 pixels
+			sami.move(3);
+			// 7. Turn the robot 1 degree
+			sami.turn(1);
+			// 8. If the counter i is divisible by 20 (hint: use mod operator %)..
+			if (i % 20 == 0) {
 
-        // 3. Put the robot's pen down
-
-        // 4. Set the robot’s speed to 10
-
-        // 5. Do everything below here 360 times (use i as the counter)
-
-	        // 6. Move the robot 3 pixels
-
-       //  7. Turn the robot 1 degree
-
-        // 8. If the counter i is divisible by 20 (hint: use mod operator  %)..
-
-		// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
-
-				// 10. Move the robot 1 pixel
-
-				// 11. Turn the robot 1 degree
-}
+				// 9. do steps 10 and 11 exactly 360 times (use j as the counter)
+				for (int j = 0; j < 360; j++) {
+					sami.setRandomPenColor();
+					// 10. Move the robot 1 pixel
+					sami.move(1);
+					// 11. Turn the robot 1 degree
+					sami.turn(1);
+				}
+			}
+		}
+	}
 }
